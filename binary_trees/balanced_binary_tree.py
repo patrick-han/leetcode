77f5d05ebf81697cb11c12c64671ac4d69ef9d2f
Patrick Han <patric1khan@gmail.com>
2020-10-07 10:45:04 -0700

    memoization for balanced binary tree

--- binary_trees/balanced_binary_tree.py
+++ binary_trees/balanced_binary_tree.py
@@ -5,17 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.d = {} # Memoization for the depth calculations
+        
     def maxDepth(self, node): # Helper function that finds the max height in a subtree
         if (not node):
             return 0
+        if (node in self.d): # Return memoized depth calculation
+            return self.d[node]
         else:
-            return 1 + max(self.maxDepth(node.left), self.maxDepth(node.right))
+            self.d[node] =  1 + max(self.maxDepth(node.left), self.maxDepth(node.right))
+            return self.d[node]
         
     def isBalanced(self, root: TreeNode) -> bool:
         # A tree is balanced if abs(height(left)-height(right)) <= 1
-        if (not root): # Base case, an empty node is "balanced"
+        if (not root):
             return True
-        elif (abs(self.maxDepth(root.left) - self.maxDepth(root.right)) > 1): # Sub problem to solve if the two subtrees are balanced
-            return False
-        else:
-            return self.isBalanced(root.left) and self.isBalanced(root.right) # Recursive call to both subtrees
+        
+        offByOneOrLess = abs(self.maxDepth(root.left) - self.maxDepth(root.right)) <= 1
+        return offByOneOrLess and self.isBalanced(root.left) and self.isBalanced(root.right)
